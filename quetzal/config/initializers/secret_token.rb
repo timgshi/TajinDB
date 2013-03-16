4a4c548d7795304d1119ea1f34373a3e0293c78d
Tim Shi <timgshi@gmail.com>
2013-03-16 12:41:46 -0700

    started the rails project

---
+++ quetzal/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quetzal::Application.config.secret_token = '3400641b891f66c27441c8277a5465e6765d6b16cb8f43d66fc8e62915c5a318ea733f75c9f38e1e6aaaab9c9bbca6262d8c211fcc47ca06138a8597d99e4360'
